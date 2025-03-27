2da77ee50776c3f8f3a09c436d7b94ab36ce7c94
Fiififyic <fiififyic@gmail.com>
2025-03-27 16:31:32 +0000

    Add suit-app/.next/static/chunks/src_app_(auth)_signin_page_tsx_1fa45077._.js 1743093090915

---
+++ suit-app/.next/static/chunks/src_app_(auth)_signin_page_tsx_1fa45077._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_(auth)_signin_page_tsx_1fa45077._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_(auth)_signin_page_tsx_1fa45077._.js",
+  "chunks": [
+    "static/chunks/_d79b6d35._.js"
+  ],
+  "source": "dynamic"
+});
